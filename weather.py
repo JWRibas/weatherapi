7a6febb3b0a35e99b90a951b1deae3df6ffd166a
Junior Ribas <133827028+JWRibas@users.noreply.github.com>
2023-07-12 19:01:27 -0300

    tk commit

--- weather.py
+++ weather.py
@@ -1,29 +1,31 @@
+import tkinter as tk
 import requests
 from dotenv import load_dotenv
 import os
-from pprint import pprint
 
 load_dotenv()
 
 def get_current_weather():
-    print('\n***Consulta o Clima Agora***\n')
-
-    city = input('\nPor favor coloque o nome da Cidade\n')
-
-
+    city = city_entry.get()
     request_url = f'https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API")}&q={city}&lang={"pt_br"}&units=metric'
-
-    
-    
-    #print(request_url)
-
     weather_data = requests.get(request_url).json()
+    result_label['text'] = f'{weather_data["name"]}\n' \
+                           f'Temperatura de: {weather_data["main"]["temp"]} Celsius\n' \
+                           f'Sensação Termica: {weather_data["main"]["feels_like"]} Celsius e com {weather_data["weather"][0]["description"]}.'
 
-    #pprint(weather_data)
-    print(f'\nClima hoje para: {weather_data["name"]}')
-    print(f'\nÉ Temperatura de: {weather_data["main"]["temp"]} Celsius')
-    print(f'\nA Sensação Termica está em: {weather_data["main"]["feels_like"]} Celsius e com {weather_data["weather"][0]["description"]}.')
-    
+root = tk.Tk()
+root.title('Consulta o Clima Agora')
 
-if __name__ == "__main__":
-    get_current_weather()
+city_label = tk.Label(root, text='Por favor coloque o nome da Cidade')
+city_label.pack()
+
+city_entry = tk.Entry(root)
+city_entry.pack()
+
+submit_button = tk.Button(root, text='Consultar', command=get_current_weather)
+submit_button.pack()
+
+result_label = tk.Label(root, text='')
+result_label.pack()
+
+root.mainloop()
